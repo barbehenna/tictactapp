cdc9c283984117d0fae3393c6d5e5f3211525e72
Alton Barbehenn <altonbarbehenn@gmail.com>
2019-08-13 18:16:00 -0500

    fixed win condition bugs caused by if-elses (guess)

--- Board/Board.cpp
+++ Board/Board.cpp
@@ -129,49 +129,55 @@ bool Board::isBoardFull( void ) {
 }
 
 int Board::whoWon( void ) {
+	// Rows
+	// Top
 	if (board[0][0] == board[0][1] && board[0][1] == board[0][2]) {
 		if (board[0][0] != 0) {
 			return board[0][0];
 		}
 	}
-
-	else if (board[1][0] == board[1][1] && board[1][1] == board[1][2]) {
+	// Middle
+	if (board[1][0] == board[1][1] && board[1][1] == board[1][2]) {
 		if (board[1][0] != 0) {
 			return board[1][0];
 		}
 	}
-
-	else if (board[2][0] == board[2][1] && board[2][1] == board[2][2]) {
+	// Bottom
+	if (board[2][0] == board[2][1] && board[2][1] == board[2][2]) {
 		if (board[2][0] != 0) {
 			return board[2][0];
 		}
 	}
 
-	else if (board[0][0] == board[1][0] && board[1][0] == board[2][0]) {
+	// Columns
+	// Left
+	if (board[0][0] == board[1][0] && board[1][0] == board[2][0]) {
 		if (board[0][0] != 0) {
 			return board[0][0];
 		}
 	}
-
-	else if (board[0][1] == board[1][1] && board[1][1] == board[2][1]) {
+	// Middle
+	if (board[0][1] == board[1][1] && board[1][1] == board[2][1]) {
 		if (board[0][1] != 0) {
 			return board[0][1];
 		}
 	}
-
-	else if (board[0][2] == board[1][2] && board[1][2] == board[2][2]) {
+	// Right
+	if (board[0][2] == board[1][2] && board[1][2] == board[2][2]) {
 		if (board[0][2] != 0) {
 			return board[0][2];
 		}
 	}
 
-	else if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+	// Diagonals
+	// major
+	if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
 		if (board[0][0] != 0) {
 			return board[0][0];
 		}
 	}
-
-	else if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+	// minor 
+	if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
 		if (board[0][2] != 0) {
 			return board[0][2];
 		}
